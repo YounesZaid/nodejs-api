715101c1ff4c3d4ec3624b7c24c8077f263f9e61
younes zaid <zaidyounes94@gmail.com>
2022-02-24 15:36:07 +0100

    [init-project] added controller to handle app request

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
-const { getPosts } = require("./routes/post");
+const postRoutes = require("./routes/post");
 
 const app = express();
+
+// Middleware to view executed routes
 app.use(morgan("dev"));
 
-app.get("/", getPosts);
+/** Middleware route to handle all post requests get/post ... */
+app.use("/", postRoutes);
 
 const port = 8080;
 app.listen(port, () => {
