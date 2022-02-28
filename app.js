3b32125d55497d08003c66f80377716b6edccc3b
younes zaid <zaidyounes94@gmail.com>
2022-02-28 11:55:03 +0100

    [init-project] save new post on cloud database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const postRoutes = require("./routes/post");
 
 const app = express();
@@ -16,6 +17,11 @@ mongoose.connection.on("err", (err) =>
 );
 
 // Middleware to view executed routes
+app.use(morgan("dev"));
+
+// Middleware to parse body requests
+app.use(bodyParser.json());
+
 app.use(morgan("dev"));
 
 /** Middleware route to handle all post requests get/post ... */
