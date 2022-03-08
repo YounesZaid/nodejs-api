180a9a77cec20d34fdfc5ce456a52fc455013b14
younes zaid <zaidyounes94@gmail.com>
2022-03-08 12:08:18 +0100

    [products] added product schema

---
+++ models/product.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: { type: String },
+  description: { type: String },
+  imageUrl: { type: String },
+  price: { type: Number },
+  userId: { type: String },
+});
